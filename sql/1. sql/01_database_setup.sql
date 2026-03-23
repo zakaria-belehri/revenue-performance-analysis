59f01fa5416b60bdd0534db49cddfcbd7b8c0594
Zakaria Belehri <zakariabelehri05@gmail.com>
2026-03-23 21:25:56 +0100

    Add database setup SQL

---
+++ sql/1. sql/01_database_setup.sql
@@ -0,0 +1,9 @@
+-- =========================================
+-- 01. DATABASE SETUP
+-- Objective: Create the project database and schemas
+-- =========================================
+
+create database ECOMMERCE_PORTFOLIO;
+
+CREATE SCHEMA IF NOT EXISTS raw;
+CREATE SCHEMA IF NOT EXISTS analytics;
